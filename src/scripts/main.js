3ea4f6a9442ce3335bfdfa97eb82c9be5e54a142
Boris Mund <boris17527684@gmail.com>
2025-11-10 23:17:55 +0300

    not

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,81 @@
 'use strict';
-
+/* eslint-disable */
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const toSeeMesagge = document.createElement('div');
+  document.body.append(toSeeMesagge);
+  toSeeMesagge.classList.add('notification');
+
+  const h2 = document.createElement('h2');
+  toSeeMesagge.append(h2);
+  h2.textContent = title;
+
+  const p = document.createElement('p');
+  toSeeMesagge.append(p);
+  p.textContent = description;
+  console.log(toSeeMesagge);
+
+  toSeeMesagge.style.position = 'fixed';
+  toSeeMesagge.style.top = posTop + 'px';
+  toSeeMesagge.style.right = posRight + 'px';
+
+  if (type === 'success') {
+    toSeeMesagge.classList.add('success');
+  }
+  if (type === 'error') {
+    toSeeMesagge.classList.add('error');
+  }
+  if (type === 'warning') {
+    toSeeMesagge.classList.add('warning');
+  }
+
+  setTimeout(() => {
+    toSeeMesagge.style.visibility = 'hidden';
+  }, 2000);
 };
+// Чтобы сообщить пользователю об успешной операции или об ошибках, сайты обычно показывают всплывающие уведомления — сообщения, которые появляются на экране на короткое время.
+
+// Твоя задача:
+
+// Создать функцию pushNotification, которая принимает:
+
+//TODO координаты сообщения,
+
+//TODO заголовок,
+
+//TODO описание,
+
+//TODO и тип уведомления (success, error, warning).
+
+// Функция pushNotification должна:
+
+//TODO Создать элемент для отображения сообщения. // СДЕЛАЛ
+
+//TODO Добавить этот элемент в документ (document). СДЕЛАЛ
+
+// Через 2 секунды скрыть сообщение (не удалять из DOM).
+
+// Сообщение — это блочный элемент с классом:
+
+//TODO notification + дополнительный класс в зависимости от типа
+
+// Например:
+
+//TODO notification success
+
+//TODO notification error
+
+//TODO notification warning
+
+//TODO Уведомление должно содержать:
+
+//TODO заголовок (<h2>) с классом title
+
+//TODO описание (<p>)
+
+// Используй setTimeout, чтобы выполнить скрытие элемента через 2 секунды.
+
+// ⚠️ Важно: не удаляй элементы из DOM!
+// Просто спрячь их с помощью стиля, например:
 
 pushNotification(
   10,
